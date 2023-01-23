df26fe117ecf1d0b831925eb4f74ba3178adef33
Ysrael Sacrati <85563735+ysrael12@users.noreply.github.com>
2023-01-22 21:48:05 -0300

    Update Search.jsx

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 
 class Form extends React.Component {
