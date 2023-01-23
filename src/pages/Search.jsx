bb80f636936b9e28318b83781fc16cfaaee05719
Ysrael Sacrati <85563735+ysrael12@users.noreply.github.com>
2023-01-22 21:46:02 -0300

    Update Search.jsx

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,20 +2,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
-function Search(props){
+class Form extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {value : ' '};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    return(
-        <Form action={"https://images-api.nasa.gov/search?q="+ props.search }method="post" className="d-flex">
-        <Form.Control
-          type="search"
-          placeholder="Search"
-          className="me-2"
-          aria-label="Search"
-        />
-        <Button variant="outline-success">Search</Button>
-      </Form>
+  handleChange(event){
+    this.setState({value : event.target.value});
+  }
+
+  handleSubmit(event){
+    axios.get('https://images-api.nasa.gov/search?q='+this.state.value)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Nome:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
     );
+  }
 }
-
-export default Search;
